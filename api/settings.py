3da9e98baa0626dcfb472a74bd914700103ed2d1
Nick Giegerich <nickgiegerich@u.boisestate.edu>
2021-10-20 13:54:05 -0600

    added static root

--- api/settings.py
+++ api/settings.py
@@ -128,6 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
